1daa50db3bdc0b91f65d07c56fa876562f052776
afdul <afdul100ahmad@gmail.com>
2024-09-13 17:32:22 +0530

    update deps

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const theme = createTheme({
+  palette: {
+    mode: "dark", // Set to 'light' or 'dark'
+    primary: {
+      main: "#222045", // Your primary color
+    },
+    secondary: {
+      main: "#dc004e", // Your secondary color
+    },
+    background: {
+      default: "#0e0b33", // Background color
+      paper: "#0e0b33", // Paper (card) background color
+    },
+    text: {
+      primary: "#ffffff", // Text color
+      secondary: "#bbbbbb", // Secondary text color
+    },
+  },
+});
+
+createRoot(document.getElementById("root")).render(
+  <ThemeProvider theme={theme}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ThemeProvider>
+);
